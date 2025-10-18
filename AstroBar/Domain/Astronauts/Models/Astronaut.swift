2620073793436b82fa2bedf44605d490f50b325a
Jakub Tomana <jakub.tomana@protonmail.com>
2025-10-18 18:43:02 +0200

    fix: Correct type, remove empty Info.plist file, use UUID as astronaut id instead of their name

---
+++ AstroBar/Domain/Astronauts/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  AstroBar
+//
+//  Created by Jakub Tomana on 16/10/2025.
+//
+
+import Foundation
+
+struct Astronaut: Identifiable {
+    let id = UUID()
+    let name: String
+    let craft: String
+}
